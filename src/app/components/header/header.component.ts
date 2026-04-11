074f0d4f8eac821da857a6126e0cadfcbdf6122d
Karol Nowak <karolnowak3217@gmail.com>
2026-04-11 12:13:10 +0200

    ToDoList + Hamburger Menu

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,17 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
 import {RouterLink, RouterLinkActive} from '@angular/router';
 import {MaterialModule} from '../../materials/material.core';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-header',
   imports: [
     RouterLink,
     RouterLinkActive,
-    MaterialModule
+    MaterialModule,
+    NgClass
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+
+  isOpen = signal(false);
+
+  toggleMenu() {
+    this.isOpen.update(val => !val)
+  }
 
 }
